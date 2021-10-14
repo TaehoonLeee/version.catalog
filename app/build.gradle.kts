43dfab13e8691c5cef351e09e5de8e2ed950a724
TaehoonLeee <lyc9290@gmail.com>
2021-10-14 12:11:14 +0900

    dependabot test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.1.0-alpha03")
     debugImplementation("androidx.compose.ui:ui-tooling:1.1.0-alpha03")
 }
