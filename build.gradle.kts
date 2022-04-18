4d07aeec5856bf1e066eb2a5ea50a94d4689758b
TaehoonLeee <lyc9290@gmail.com>
2022-04-18 11:59:36 +0900

    version catalog has been stable over 7.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        val deps = project.extensions.getByType<VersionCatalogsExtension>().named("deps") as org.gradle.accessors.dm.LibrariesForDeps
-        classpath(deps.android.gradle.plugin)
-        classpath(deps.kotlin.gradle.plugin)
+        classpath(deps.android.gradle)
+        classpath(deps.kotlin.gradle)
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
